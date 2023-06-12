732961231f69ddcdefc9025a14f637292cc3a32d
Eduardo Sanchez <91806621+Layloe@users.noreply.github.com>
2023-06-12 09:42:53 -0700

    debugged cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import CitySearch from './CitySearch';
+import AirQualityCard from './AirQualityCard'
+import PullutantInfo from './PollutantInfo';
+import AirQualityLevelsTable from './AirQualityLevelsTable';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
 
 function App() {
+const [airQualityData, setAirQualityData] = useState(null)
+const [error, setError] = useState(null)
+
+  const getAirQuality = async (city) => {
+    try {
+      const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_AQI_API_TOKEN}`)
+      const data = await response.json()
+      console.log(data)
+      if(response.ok && data.status === 'ok') {
+        setAirQualityData(data.data)
+      } else {
+        setError("Sorry, we couldn't find the city you were looking for. Try another location nearby or ensure your spelling is correct.")
+      }
+    } catch(error) {
+      console.error("network error:", error)
+      setError('Sorry, something went wrong')
+      setAirQualityData(null)
+
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <h1 className="mt-5 mb-3">Air Quality Index Checker</h1>
+    
+    <CitySearch getAirQuality={getAirQuality} />
+
+    {error && (
+      <div className="alert alert-danger" role="alert">
+        {error}
+      </div>
+    )}
+
+    {airQualityData && (
+      <>
+      <AirQualityCard data={airQualityData} />
+      <PullutantInfo pollutant={airQualityData.dominentpol}/>
+      </>
+    )}
+    <AirQualityLevelsTable />
+    <p>Location-specific API data sourced from the World Air Quality Index Project. <a href='https://aqicn.org/api/'>LINK</a></p>
     </div>
   );
 }
